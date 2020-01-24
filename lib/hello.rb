2bcacac55342f4904261c5a93a1d33daff3c56e1
Karen Daniela <kdaniimora@gmail.com>
2020-01-24 22:01:54 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def hello_t(array)
   if block_given?
     i = 0
@@ -11,9 +12,29 @@ def hello_t(array)
   else
     puts "Hey! No block was given!"
   end
+=======
+def hello_t (array)
+ if block_given?
+  i = 0
+ 
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+array
+else
+  puts "Hey! no block was given"
+>>>>>>> cc3d626265a482301f2f931328e36ecb4a3cd18a
 end
+end 
 
 # call your method here!
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
+
 
 ["Tim", "Tom", "Jim"].each do |name|
   if name.start_with?("T")
